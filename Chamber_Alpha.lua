59cae6729d95d234bd25e18332b2c25345791abd
Centralan <Jessegauthier070296@gmail.com>
2024-01-03 13:38:50 -0500

    Update Chamber_Alpha.lua

--- Chamber_Alpha.lua
+++ Chamber_Alpha.lua
@@ -33,3 +33,29 @@ registerHook("REGION_ENTER", "chamer_a_1", "chambers-a_17");
 registerHook("REGION_ENTER", "chamer_a_1", "chambers-a_18");
 registerHook("REGION_ENTER", "chamer_a_1", "chambers-a_19");
 
+--Alpha 2
+
+function chamer_a_2(data)
+        local player = Player:new(data.player);
+        player:setHealth(20);
+                player:teleport(chamber_a);
+	        player:sendMessage("&7The sanity is starting to set in...");
+                player:playSound('ITEM_CHORUS_FRUIT_TELEPORT', 1, 0.5);
+end
+
+registerHook("REGION_ENTER", "chamer_a_2", "chambers-a_20");
+registerHook("REGION_ENTER", "chamer_a_2", "chambers-a_21");
+registerHook("REGION_ENTER", "chamer_a_2", "chambers-a_22");
+registerHook("REGION_ENTER", "chamer_a_2", "chambers-a_23");
+registerHook("REGION_ENTER", "chamer_a_2", "chambers-a_24");
+registerHook("REGION_ENTER", "chamer_a_2", "chambers-a_25");
+registerHook("REGION_ENTER", "chamer_a_2", "chambers-a_26");
+registerHook("REGION_ENTER", "chamer_a_2", "chambers-a_27");
+registerHook("REGION_ENTER", "chamer_a_2", "chambers-a_28");
+registerHook("REGION_ENTER", "chamer_a_2", "chambers-a_29");
+registerHook("REGION_ENTER", "chamer_a_2", "chambers-a_30");
+registerHook("REGION_ENTER", "chamer_a_2", "chambers-a_31");
+registerHook("REGION_ENTER", "chamer_a_2", "chambers-a_32");
+registerHook("REGION_ENTER", "chamer_a_2", "chambers-a_33");
+registerHook("REGION_ENTER", "chamer_a_2", "chambers-a_34");
+registerHook("REGION_ENTER", "chamer_a_2", "chambers-a_35");
