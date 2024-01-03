4be1583f0ce4599b12e90bfd01b0b0f0c6b90df8
Centralan <Jessegauthier070296@gmail.com>
2024-01-03 18:01:11 -0500

    added chamber b TP

--- lobby.lua
+++ lobby.lua
@@ -28,15 +28,26 @@ local chamber_a = Location:new(world, 5000.0, 100.0, 1000.0);
 chamber_a:setYaw(-178.8);
 chamber_a:setPitch(12.9);
 
+local chamber_b = Location:new(world, 5000.0, 100.0, 1200.0);
+chamber_a:setYaw(89.8);
+chamber_a:setPitch(6.3);
+
 function chamber_a_tp(data)
   local player = Player:new(data.player);
     player:teleport(chamber_a);
-    player:addPermission("runsafe.chambers.alpha");
     player:sendTitle("Welcome To", "Chamber Alpha");
     player:playSound('ENTITY_EVOCATION_ILLAGER_CAST_SPELL', 1, 0.5);
 end
 
-  registerHook("REGION_ENTER", "chamber_a_tp", "chambers-chamber_a")
+function chamber_b_tp(data)
+  local player = Player:new(data.player);
+    player:teleport(chamber_b);
+    player:sendTitle("Welcome To", "Chamber Bravo");
+    player:playSound('ENTITY_EVOCATION_ILLAGER_CAST_SPELL', 1, 0.5);
+end
+
+registerHook("REGION_ENTER", "chamber_a_tp", "chambers-chamber_a")
+registerHook("REGION_ENTER", "chamber_b_tp", "chambers-chamber_b")
 
 ----------------
 --Chamber Check--
