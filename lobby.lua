cf901844560f342e197af165d9bcf966ce4fc5c5
Centralan <Jessegauthier070296@gmail.com>
2024-01-07 15:56:21 -0500

    Update lobby.lua

--- lobby.lua
+++ lobby.lua
@@ -84,20 +84,3 @@ end
 registerHook("REGION_ENTER", "chambers_sign", "chambers-chamber_check");
 registerHook("REGION_ENTER", "chambers_sign2", "chambers-chamber_check2");
 
-----------------
---Easter Eggs --
-----------------
-
-local rab = Location:new(world, 4904.625, 89.0, 312.509);
-rab:setYaw(89.1);
-rab:setPitch(8.1);
-
-function rabbiton(data)
-	local player = Player:new(data.player);
-        player:teleport(rab);
-	player:sendTitle("", "&5&k0&dIt feels just like a dream.&5&k0");
-	player:playSound('ENTITY_EVOCATION_ILLAGER_CAST_SPELL', 1, 0.5);
-end
-
-registerHook("REGION_ENTER", "rabbiton", "chambers-rab_easter");
-
