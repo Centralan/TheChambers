1095890a77676fcb7499c493786d77445502e2f7
Centralan <Jessegauthier070296@gmail.com>
2024-01-10 18:23:12 -0500

    Create chamber_d.lua

---
+++ chamber_d.lua
@@ -0,0 +1,11 @@
+---------------------------------
+--Worlds / Teleport / Messaging--
+---------------------------------
+
+local world = World:new('chambers');
+local world2 = World:new('survival3');
+local bot = AI:new("ATLAS", "AI", "Chambers");
+
+local c_spawn = Location:new(world, 5000.491, 96.0, 500.597);
+c_spawn:setYaw(-135.9);
+c_spawn:setPitch(3.6);
