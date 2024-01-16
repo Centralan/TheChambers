c4cd9b9b4ac025fe30a80232e3ff4f9ffbaebbd2
Centralan <Jessegauthier070296@gmail.com>
2024-01-15 19:59:46 -0500

    Update chamber_delta.lua

--- chamber_delta.lua
+++ chamber_delta.lua
@@ -9,3 +9,97 @@ local bot = AI:new("ATLAS", "AI", "Chambers");
 local c_spawn = Location:new(world, 5000.491, 96.0, 500.597);
 c_spawn:setYaw(-135.9);
 c_spawn:setPitch(3.6);
+
+--delta 1
+
+local waterbucket1 = Location:new(world, 5037, 108, 1080);
+local bottle1 = Location:new(world, 5037, 108, 1083);
+
+function d1_waterbucket(data)
+	local player = Player:new(data.player);
+        player:closeInventory();
+        waterbucket1:cloneChestToPlayer(player.name);
+end
+
+function d1_bottle(data)
+	local player = Player:new(data.player);
+        player:closeInventory();
+        bottle1:cloneChestToPlayer(player.name);
+end
+
+registerHook("INTERACT", "d1_waterbucket", 54, "chambers", 5038.0, 98.0, 1101.0);
+registerHook("INTERACT", "d1_bottle", 379, "chambers", 5070.0, 101.0, 1091.0);
+
+--water bucket door
+local world = "chambers";
+local d1current = 1;
+local d1maxData = 1;
+local d1blocks = {
+        Location:new(world, 5063.0, 100.0, 1076.0),
+        Location:new(world, 5063.0, 101.0, 1076.0),
+};
+
+local world = "chambers";
+local d2current = 1;
+local d2maxData = 1;
+local d2blocks = {
+        Location:new(world, 5062.0, 104.0, 1095.0),
+        Location:new(world, 5062.0, 103.0, 1095.0),
+};
+
+local world = "chambers";
+local d3current = 1;
+local d3maxData = 1;
+local d3blocks = {
+        Location:new(world, 5048.0, 100.0, 1089.0),
+};
+
+
+function d1_set(data)
+        if d1current == d1maxData then
+                d1current = 1;
+        else
+                d1current = d1current + 1;
+        end
+        d1_1_setair();
+end
+
+function d1_1_setair()
+        for index, key in ipairs(d1blocks) do
+                key:setBlock(0, d1current);
+end
+end
+
+function d1_1_setstone()
+        for index, key in ipairs(d1blocks) do
+                key:setBlock(98, d1current);
+end
+end
+
+function d1_1_setcal()
+        for index, key in ipairs(d2blocks) do
+                key:setBlock(98, d2current);
+end
+end
+
+function d1_2_setair()
+        for index, key in ipairs(d2blocks) do
+                key:setBlock(0, d2current);
+end
+end
+
+function d1_2_setstone()
+        for index, key in ipairs(d2blocks) do
+                key:setBlock(98, d2current);
+end
+end
+
+registerHook("BLOCK_GAINS_CURRENT", "d1_1_setair", "chambers", 5050.0, 100.0, 1087.0);
+registerHook("REGION_ENTER", "d1_1_setstone", "chambers-d1_setstone");
+registerHook("REGION_ENTER", "d1_2_setcal", "chambers-d1_setstone");
+
+registerHook("INTERACT", "d1_2_setair", 143, "chambers", 5063.0, 104.0, 1095.0);
+registerHook("REGION_ENTER", "d1_2_setstone", "chambers-d1_2_setstone");
+
+
+
