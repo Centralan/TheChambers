6bd7c5909b79d948b0919ca8210bca9bd5822e6f
Centralan <Jessegauthier070296@gmail.com>
2024-01-15 20:26:56 -0500

    Update chamber_delta.lua

--- chamber_delta.lua
+++ chamber_delta.lua
@@ -48,15 +48,6 @@ local d2blocks = {
         Location:new(world, 5062.0, 103.0, 1095.0),
 };
 
---cal repalce
-local world = "chambers";
-local d3current = 1;
-local d3maxData = 1;
-local d3blocks = {
-        Location:new(world, 5048.0, 100.0, 1089.0),
-};
-
-
 function d1_set(data)
         if d1current == d1maxData then
                 d1current = 1;
@@ -78,11 +69,6 @@ function d1_1_setstone()
 end
 end
 
-function d1_1_setcal()
-        for index, key in ipairs(d3blocks) do
-                key:setBlock(380, d3current);
-end
-end
 
 function d1_2_setair()
         for index, key in ipairs(d2blocks) do
@@ -98,7 +84,6 @@ end
 
 registerHook("BLOCK_GAINS_CURRENT", "d1_1_setair", "chambers", 5050.0, 97.0, 1086.0);
 registerHook("REGION_ENTER", "d1_1_setstone", "chambers-d1_setstone");
-registerHook("REGION_ENTER", "d1_1_setcal", "chambers-d1_setstone");
 
 registerHook("INTERACT", "d1_2_setair", 143, "chambers", 5063.0, 104.0, 1095.0);
 registerHook("REGION_ENTER", "d1_2_setstone", "chambers-d1_2_setstone");
