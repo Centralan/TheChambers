4c0f79f59698a5403efe8b76e0bf758d17e48600
Centralan <Jessegauthier070296@gmail.com>
2024-01-20 18:35:04 -0500

    Create chamber_gulf.lua

---
+++ chamber_gulf.lua
@@ -0,0 +1,7 @@
+-------------------------------
+--Worlds / Sounds / Messaging--
+-------------------------------
+
+local world = World:new('chambers');
+local bot = AI:new("ATLAS", "AI", "Chambers");
+
