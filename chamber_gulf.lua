0e2a11969dffb43ab837571ac5583c025378b359
Centralan <Jessegauthier070296@gmail.com>
2024-01-20 18:49:16 -0500

    Update chamber_gulf.lua

--- chamber_gulf.lua
+++ chamber_gulf.lua
@@ -4,4 +4,15 @@
 
 local world = World:new('chambers');
 local bot = AI:new("ATLAS", "AI", "Chambers");
+local ggear = Location:new(world, 2001, 186, 2183);
 
+function chamer_g_gear(data)
+        local player = Player:new(data.player);
+        player:setHealth(20);
+        ggear:cloneChestToPlayer(player.name);
+        player:sendMessage("&7You find some gear scattered on the ground");
+        player:sendMessage("&7It looks used...");
+        player:playSound('ENTITY_HORSE_ARMOR', 1, 0.5);
+end
+
+registerHook("REGION_ENTER", "chamer_g_gear", "chambers-g_gear")
