f286e8ffb6b464c0b84ee178c7f65c16165f6bec
Centralan <Jessegauthier070296@gmail.com>
2024-01-20 19:18:24 -0500

    Update chamber_gulf.lua

--- chamber_gulf.lua
+++ chamber_gulf.lua
@@ -10,6 +10,10 @@ local c_spawn = Location:new(world, 5000.491, 96.0, 500.597);
 c_spawn:setYaw(-135.9);
 c_spawn:setPitch(3.6);
 
+local g_spawn = Location:new(world, 2001.443, 218.0, 2131.532);
+g_spawn:setYaw(-179.7);
+g_spawn:setPitch(11.5);
+
 -----------
 --Gear--
 -----------
@@ -24,3 +28,15 @@ function chamer_g_gear(data)
 end
 
 registerHook("REGION_ENTER", "chamer_g_gear", "chambers-g_gear")
+
+function chamer_g_reset(data)
+        local player = Player:new(data.player);
+        player:teleport(g_spawn);
+        player:sendMessage("&7The spikes reach out to you");
+        player:sendMessage("&7Almost like something is controlling them...");
+        player:playSound('ENTITY_ILLUSION_ILLAGER_MIRROR_MOVE', 1, 0.5);
+        EventEngine.player.addPotionEffect(player.name, 'BLINDNESS', 100, 4);
+        EventEngine.player.addPotionEffect(player.name, 'CONFUSION', 100, 5);
+end
+
+registerHook("REGION_ENTER", "chamer_g_reset", "chambers-g_catch")
