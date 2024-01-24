83a0db6e0b1c8997d449d35531b97de5a7056f6a
Centralan <Jessegauthier070296@gmail.com>
2024-01-24 00:32:54 -0500

    started boss bar progression

--- chamber_alpha.lua
+++ chamber_alpha.lua
@@ -119,3 +119,187 @@ function chamer_a_complete(data)
 end
 
 registerHook("REGION_ENTER", "chamer_a_complete", "chambers-a_complete");
+
+
+-- Boss Bar Progress Room 1
+local a1BossBar = BossBar:new()
+local a2BossBar = BossBar:new()
+local a3BossBar = BossBar:new()
+local a4BossBar = BossBar:new()
+local a5BossBar = BossBar:new()
+local a6BossBar = BossBar:new()
+local a8BossBar = BossBar:new()
+local a9BossBar = BossBar:new()
+local a11BossBar = BossBar:new()
+local a12BossBar = BossBar:new()
+
+function a1(data)
+	local player = Player:new(data.player);
+        a1BossBar:createBossBar("Deception: Room Progress")     
+        a1BossBar:setColour("red")
+        a1BossBar:addPlayer(data.player)
+        a1BossBar:setStyle("segmented10")
+        a1BossBar:setProgress(0.1)
+        a1BossBar:setVisible(true)
+end
+
+function a2(data)
+	local player = Player:new(data.player);
+        a2BossBar:createBossBar("Deception: Room Progress")     
+        a2BossBar:setColour("red")
+        a2BossBar:addPlayer(data.player)
+        a2BossBar:setStyle("segmented10")
+        a2BossBar:setProgress(0.2)
+        a2BossBar:setVisible(true)
+end
+
+function a3(data)
+	local player = Player:new(data.player);
+        a3BossBar:createBossBar("Deception: Room Progress")     
+        a3BossBar:setColour("red")
+        a3BossBar:addPlayer(data.player)
+        a3BossBar:setStyle("segmented10")
+        a3BossBar:setProgress(0.3)
+        a3BossBar:setVisible(true)
+end
+
+function a4(data)
+	local player = Player:new(data.player);
+        a4BossBar:createBossBar("Deception: Room Progress")     
+        a4BossBar:setColour("red")
+        a4BossBar:addPlayer(data.player)
+        a4BossBar:setStyle("segmented10")
+        a4BossBar:setProgress(0.4)
+        a4BossBar:setVisible(true)
+end
+
+function a5(data)
+	local player = Player:new(data.player);
+        a5BossBar:createBossBar("Deception: Room Progress")     
+        a5BossBar:setColour("yellow")
+        a5BossBar:addPlayer(data.player)
+        a5BossBar:setStyle("segmented10")
+        a5BossBar:setProgress(0.5)
+        a5BossBar:setVisible(true)
+end
+
+function a6(data)
+	local player = Player:new(data.player);
+        a6BossBar:createBossBar("Deception: Room Progress")     
+        a6BossBar:setColour("yellow")
+        a6BossBar:addPlayer(data.player)
+        a6BossBar:setStyle("segmented10")
+        a6BossBar:setProgress(0.6)
+        a6BossBar:setVisible(true)
+end
+
+function a8(data)
+	local player = Player:new(data.player);
+        a8BossBar:createBossBar("Deception: Room Progress")     
+        a8BossBar:setColour("yellow")
+        a8BossBar:addPlayer(data.player)
+        a8BossBar:setStyle("segmented10")
+        a8BossBar:setProgress(0.7)
+        a8BossBar:setVisible(true)
+end
+
+function a9(data)
+	local player = Player:new(data.player);
+        a9BossBar:createBossBar("Deception: Room Progress")     
+        a9BossBar:setColour("yellow")
+        a9BossBar:addPlayer(data.player)
+        a9BossBar:setStyle("segmented10")
+        a9BossBar:setProgress(0.8)
+        a9BossBar:setVisible(true)
+end
+
+function a11(data)
+	local player = Player:new(data.player);
+        a11BossBar:createBossBar("Deception: Room Progress")     
+        a11BossBar:setColour("yellow")
+        a11BossBar:addPlayer(data.player)
+        a11BossBar:setStyle("segmented10")
+        a11BossBar:setProgress(0.9)
+        a11BossBar:setVisible(true)
+end
+
+function a12(data)
+	local player = Player:new(data.player);
+        a12BossBar:createBossBar("Deception: Room Progress")     
+        a12BossBar:setColour("green")
+        a12BossBar:addPlayer(data.player)
+        a12BossBar:setStyle("segmented10")
+        a12BossBar:setProgress(1)
+        a12BossBar:setVisible(true)
+end
+
+function a1e(data)
+	local player = Player:new(data.player);
+        a1BossBar:setVisible(false)
+end
+
+function a2e(data)
+	local player = Player:new(data.player);
+        a2BossBar:setVisible(false)
+end
+
+function a3e(data)
+	local player = Player:new(data.player);
+        a3BossBar:setVisible(false)
+end
+
+function a4e(data)
+	local player = Player:new(data.player);
+        a4BossBar:setVisible(false)
+end
+
+function a5e(data)
+	local player = Player:new(data.player);
+        a5BossBar:setVisible(false)
+end
+
+function a6e(data)
+	local player = Player:new(data.player);
+        a6BossBar:setVisible(false)
+end
+
+function a8e(data)
+	local player = Player:new(data.player);
+        a8BossBar:setVisible(false)
+end
+
+function a9e(data)
+	local player = Player:new(data.player);
+        a9BossBar:setVisible(false)
+end
+
+function a11e(data)
+	local player = Player:new(data.player);
+        a11BossBar:setVisible(false)
+end
+
+function a12e(data)
+	local player = Player:new(data.player);
+        a12BossBar:setVisible(false)
+end
+
+registerHook("REGION_ENTER", "a1", "chambers-a_b1_1");
+registerHook("REGION_ENTER", "a1e", "chambers-a_b1_2");
+registerHook("REGION_ENTER", "a2", "chambers-a_b1_2");
+registerHook("REGION_ENTER", "a2e", "chambers-a_b1_3");
+registerHook("REGION_ENTER", "a3", "chambers-a_b1_3");
+registerHook("REGION_ENTER", "a3e", "chambers-a_b1_4");
+registerHook("REGION_ENTER", "a4", "chambers-a_b1_4");
+registerHook("REGION_ENTER", "a4e", "chambers-a_b1_5");
+registerHook("REGION_ENTER", "a5", "chambers-a_b1_5");
+registerHook("REGION_ENTER", "a5e", "chambers-a_b1_6");
+registerHook("REGION_ENTER", "a6", "chambers-a_b1_6");
+registerHook("REGION_ENTER", "a6e", "chambers-a_b1_8");
+registerHook("REGION_ENTER", "a8", "chambers-a_b1_8");
+registerHook("REGION_ENTER", "a8e", "chambers-a_b1_9");
+registerHook("REGION_ENTER", "a9", "chambers-a_b1_9");
+registerHook("REGION_ENTER", "a9e", "chambers-a_b1_11");
+registerHook("REGION_ENTER", "a11", "chambers-a_b1_11");
+registerHook("REGION_ENTER", "a11e", "chambers-a_b1_12");
+registerHook("REGION_ENTER", "a12", "chambers-a_b1_12");
+registerHook("REGION_ENTER", "a12e", "chambers-a1_b12_e");
